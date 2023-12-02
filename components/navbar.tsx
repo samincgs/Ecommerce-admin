00f6deec989a7e15238c11421ea99a2189dc75b0
samin <samincgs@gmail.com>
2023-12-01 21:33:42 -0500

    Popover has been properly made, users can now create new stores and switch to different ones

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,11 +1,26 @@
-import { UserButton } from '@clerk/nextjs';
-import MainNav from './main-nav';
+import { UserButton, auth } from '@clerk/nextjs';
+import MainNav from '@/components/main-nav';
+import StoreSwitcher from '@/components/store-switcher';
+import { redirect } from 'next/navigation';
+import prismadb from '@/lib/prismadb';
 
-const Navbar = () => {
+const Navbar = async () => {
+  const { userId } = auth();
+
+  if (!userId) {
+    redirect('/sign-in');
+  }
+
+  const stores = await prismadb.store.findMany({
+    where: {
+      userId,
+    },
+  });
+
   return (
     <div className='border-b'>
-      <div className='h-16 flex items-center px-4'>
-        <div className=''>this will be a store switcher</div>
+      <div className='h-16 flex items-center  px-4'>
+        <StoreSwitcher items={stores} />
         <MainNav className='mx-6' />
         <div className='ml-auto space-x-4 flex items-center'>
           <UserButton afterSignOutUrl='/' />
