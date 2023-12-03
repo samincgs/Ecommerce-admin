0c330e03f4bb286727f8c52d3a73ea8a5b57a5b5
samin <samincgs@gmail.com>
2023-12-02 19:19:02 -0500

    working on settings page

---
+++ app/(dashboard)/[storeId]/settings/page.tsx
@@ -0,0 +1,32 @@
+import prismadb from '@/lib/prismadb';
+import { auth } from '@clerk/nextjs';
+import { redirect } from 'next/navigation';
+
+interface SettingsPageProps {
+  params: {
+    storeId: string;
+  };
+}
+
+const SettingsPage: React.FC<SettingsPageProps> = async ({ params }) => {
+  const { userId } = auth();
+
+  if (!userId) {
+    redirect('/sign-in');
+  }
+
+  const store = await prismadb.store.findFirst({
+    where: {
+      id: params.storeId,
+      userId,
+    },
+  });
+
+  if (!store) {
+    redirect('/');
+  }
+
+  return <div className=''></div>;
+};
+
+export default SettingsPage;
