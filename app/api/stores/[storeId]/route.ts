86d3aa176f423e137923783a0d3fb2a6138cae0f
samin <samincgs@gmail.com>
2023-12-03 04:20:21 -0500

    Users can now delete the stores they have created

--- app/api/stores/[storeId]/route.ts
+++ app/api/stores/[storeId]/route.ts
@@ -23,7 +23,7 @@ export async function PATCH(
       return new NextResponse('Store id is required', { status: 400 });
     }
 
-    const store = await prismadb.store.update({
+    const store = await prismadb.store.updateMany({
       where: {
         id: params.storeId,
         userId,
@@ -54,7 +54,7 @@ export async function DELETE(
       return new NextResponse('Store id is required', { status: 400 });
     }
 
-    const store = prismadb.store.delete({
+    const store = await prismadb.store.deleteMany({
       where: {
         id: params.storeId,
         userId,
--- components/settings-form.tsx
+++ components/settings-form.tsx
@@ -21,6 +21,7 @@ import { Input } from '@/components/ui/input';
 import toast from 'react-hot-toast';
 import axios from 'axios';
 import { useParams, useRouter } from 'next/navigation';
+import AlertModal from '@/components/modals/alert-modal';
 
 interface SettingsFormProps {
   initialData: Store;
@@ -57,8 +58,30 @@ const SettingsForm: React.FC<SettingsFormProps> = ({ initialData }) => {
     console.log(values);
   };
 
+  const onDelete = async () => {
+    try {
+      setLoading(true);
+
+      await axios.delete(`/api/stores/${params.storeId}`);
+      router.refresh();
+      router.push('/');
+      toast.success('Store deleted.');
+    } catch (error) {
+      toast.error('Make sure you removed all products and categories first.');
+    } finally {
+      setLoading(false);
+      setOpen(false);
+    }
+  };
+
   return (
     <>
+      <AlertModal
+        isOpen={open}
+        onClose={() => setOpen(false)}
+        onConfirm={onDelete}
+        loading={loading}
+      />
       <div className=' flex items-center justify-between'>
         <Heading title='Settings' description='Manage store preferences' />
         <Button
