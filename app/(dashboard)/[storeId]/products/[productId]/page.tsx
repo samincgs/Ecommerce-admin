7eac1920b023a27d9eb829641e36ebf79af64d99
samin <samincgs@gmail.com>
2023-12-11 05:02:50 -0500

    Started the Products page and implemented the multiple image posting functionality

---
+++ app/(dashboard)/[storeId]/products/[productId]/page.tsx
@@ -0,0 +1,22 @@
+import ProductForm from '../components/product-form';
+import prismadb from '@/lib/prismadb';
+
+const ProductPage = async ({ params }: { params: { productId: string } }) => {
+  const product = await prismadb.product.findUnique({
+    where: {
+      id: params.productId,
+    },
+    include: {
+      images: true,
+    },
+  });
+
+  return (
+    <div className='flex-col'>
+      <div className='flex-1 space-y-4 p-8 pt-6'>
+        <ProductForm initialData={product} />
+      </div>
+    </div>
+  );
+};
+export default ProductPage;
---
+++ app/(dashboard)/[storeId]/products/components/product-client.tsx
@@ -0,0 +1,45 @@
+'use client';
+
+import { Plus } from 'lucide-react';
+import { useParams, useRouter } from 'next/navigation';
+
+import Heading from '@/components/ui/heading';
+import ApiList from '@/components/ui/api-list';
+import { Button } from '@/components/ui/button';
+import { Separator } from '@/components/ui/separator';
+import { ProductColumn, columns } from './columns';
+import { DataTable } from '@/components/ui/data-table';
+
+interface ProductClientProps {
+  data: ProductColumn[];
+}
+
+const ProductClient: React.FC<ProductClientProps> = ({ data }) => {
+  const router = useRouter();
+  const params = useParams();
+
+  const pushToNewProduct = () => {
+    router.push(`/${params.storeId}/products/new`);
+  };
+
+  return (
+    <>
+      <div className='flex items-center justify-between'>
+        <Heading
+          title={`Products (${data.length})`}
+          description='Manage products for your store'
+        />
+        <Button onClick={pushToNewProduct}>
+          <Plus className='w-4 h-4 mr-2' />
+          Add New
+        </Button>
+      </div>
+      <Separator />
+      <DataTable searchKey='label' columns={columns} data={data} />
+      <Heading title='API' description='API calls for Products' />
+      <Separator />
+      <ApiList entityName='products' entityIdName='productId' />
+    </>
+  );
+};
+export default ProductClient;
