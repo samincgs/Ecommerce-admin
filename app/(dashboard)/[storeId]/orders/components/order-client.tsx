4459ccbcacd9bda066090f0cc7c1d9dd7e2d4d61
samin <samincgs@gmail.com>
2023-12-12 05:25:55 -0500

    Order page design is now fully complete

---
+++ app/(dashboard)/[storeId]/orders/components/order-client.tsx
@@ -0,0 +1,24 @@
+'use client';
+
+import Heading from '@/components/ui/heading';
+import { Separator } from '@/components/ui/separator';
+import { OrderColumn, columns } from './columns';
+import { DataTable } from '@/components/ui/data-table';
+
+interface OrderClientProps {
+  data: OrderColumn[];
+}
+
+const OrderClient: React.FC<OrderClientProps> = ({ data }) => {
+  return (
+    <>
+      <Heading
+        title={`Orders (${data.length})`}
+        description='Manage orders for your store'
+      />
+      <Separator />
+      <DataTable searchKey='products' columns={columns} data={data} />
+    </>
+  );
+};
+export default OrderClient;
