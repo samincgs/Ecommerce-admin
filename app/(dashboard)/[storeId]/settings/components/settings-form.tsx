7460cf39dc1c48b1964f09de714234dcda795a24
samin <samincgs@gmail.com>
2023-12-13 02:22:45 -0500

    fixed public api url

--- app/(dashboard)/[storeId]/settings/components/settings-form.tsx
+++ app/(dashboard)/[storeId]/settings/components/settings-form.tsx
@@ -129,7 +129,7 @@ const SettingsForm: React.FC<SettingsFormProps> = ({ initialData }) => {
       <Separator />
       <ApiAlert
         title='NEXT_PUBLIC_API_URL'
-        description={`${origin}/api/${params.storeId}}`}
+        description={`${origin}/api/${params.storeId}`}
         variant='public'
       />
     </>
